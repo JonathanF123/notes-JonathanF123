a8401592210342feb2a00b6d6b0d1f87e5284da9
JonathanF123 <146701193+JonathanF123@users.noreply.github.com>
2024-05-17 09:08:21 +0700

    update

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -5,6 +5,8 @@ class Note {
   final String title;
   final String description;
   String? imageUrl;
+  double? latitude;
+  double? longitude;
   Timestamp? createdAt;
   Timestamp? updatedAt;
 
@@ -13,27 +15,33 @@ class Note {
     required this.title,
     required this.description,
     this.imageUrl,
+    this.latitude,
+    this.longitude,
     this.createdAt,
     this.updatedAt,
   });
 
   factory Note.fromDocument(DocumentSnapshot doc) {
-    Map data = doc.data() as Map;
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
     return Note(
       id: doc.id,
       title: data['title'],
       description: data['description'],
-      imageUrl: data['imageUrl'],
+      imageUrl: data['image_url'],
+      latitude: data['latitude'] as double,
+      longitude: data['longitude'] as double,
       createdAt: data['created_at'] as Timestamp,
       updatedAt: data['updated_at'] as Timestamp,
     );
   }
 
-  Map toDocument() {
+  Map<String, dynamic> toDocument() {
     return {
       'title': title,
       'description': description,
-      'imageUrl': imageUrl,
+      'image_url': imageUrl,
+      'latitude': latitude,
+      'longitude': longitude,
       'created_at': createdAt,
       'updated_at': updatedAt,
     };
